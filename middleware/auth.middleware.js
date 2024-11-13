236ed0cba91c4b30f5f7d1426a922a639b59629c
JEEL RUPARELIYA <108856045+Jeelislive@users.noreply.github.com>
2024-11-13 11:05:32 +0530

    middleware changes

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -6,7 +6,8 @@ import env from "dotenv";
 env.config();
 
 const isAuthenticated = TryCatch(async (req, res, next) => {
-   const token = req.cookies[NOCAPPTOKEN];
+    const token = req.headers.authorization?.split(' ')[1] || req.cookies[NOCAPPTOKEN];
+    // console.log(token);
    
    if (!token) return next(new ErrorHandler("Please login to access this route", 401));
 
